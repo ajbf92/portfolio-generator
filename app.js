100e2a0bb0cea4a093f440a72864eebcf94852f6
Alex Betancourt <alex@Alexs-MacBook-Pro.local>
2021-12-08 17:11:46 -0500

    generate html page with user input

--- app.js
+++ app.js
@@ -1,16 +1,60 @@
+// const fs = require('fs');
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// // const name = profileDataArgs[0];
+// // const github = profileDataArgs[1];
+
+// const [name, github] = profileDataArgs;
+// // const generatePage = () => 'Name: Jane, Github: janehub';
+
+// // const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
+// // console.log(generatePage('Jane', 'janehub'));
+
+// // const generatePage = (userName, githubName) => {
+// //   return `
+// //     Name: ${userName}
+// //     GitHub: ${githubName}
+// //   `;
+// // };
+
+// const generatePage = (name, github) => {
+//   return `
+//   <!DOCTYPE html> 
+//   <html lang="en"> 
+//   <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//     <title>Portfolio Demo</title>
+//   </head>
+
+//   <body>
+//     <h1>${name}</h1>
+//     <h2><a href="https://github.com/${github}">Github</a></h2>
+//   </body>
+//   </html>
+//   `;
+// };
+
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2);
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
 
-  console.log('================');
+const [name, github] = profileDataArgs;
 
-  // Is the same as this... and can also be rewritten as this:   
-  // profileDataArr.forEach(profileItem => console.log(profileItem));
-  profileDataArr.forEach((profileItem) => {
-    console.log(profileItem)
-  });
-};
-printProfileData(profileDataArgs);
+console.log(name, github);
+
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
